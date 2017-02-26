46c7cc7ef3b6bf6623d0f304f03dcc0d65e87d4f
Mohamed Galal <abogalalmedo@gmail.com>
2017-02-26 17:57:21 +0200

    rename
    rename the class name

--- KKKK.cs
+++ KKKK.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class KKKK : MonoBehaviour {
+public class Kalmy : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
